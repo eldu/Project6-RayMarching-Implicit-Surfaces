6876001531806475eb73b5ea4179bda2e32e989c
eldu <eldu@seas.upenn.edu>
2017-02-28 14:47:15 -0500

    probably should have commited earlier

--- src/glsl/rayMarch-frag.glsl
+++ src/glsl/rayMarch-frag.glsl
@@ -7,12 +7,38 @@ struct geometry_t {
     float type;
 };
 */
+
+// Reference: https://threejs.org/docs/api/renderers/webgl/WebGLProgram.html
+// Already loaded into the fragment shader!
+// uniform mat4 viewMatrix
+// uniform vec3 cameraPosition;
+
+// Referemce: 461 Slides, TY Adam.
+
 uniform vec4 u_buffer[MAX_GEOMETRY_COUNT];
 uniform int u_count;
+uniform vec2 u_size;
+uniform mat4 u_inverseViewProjectionMatrix;
+uniform float u_far;
+
 
 varying vec2 f_uv;
 
+// Ray
+vec4 f_rayPos;
+vec4 f_rayDir;
+
 void main() {
+	// Calculate NDC	
+	float ndc_x = 2.0 * f_uv.x / u_size.x - 1.0;
+	float ndc_y = 1.0 - 2.0 * f_uv.y / u_size.y;
+	vec4 f_ndc = vec4(ndc_x, ndc_y, 1.0, 1.0);
+
+	// Calculate Ray
+	vec4 P = u_inverseViewProjectionMatrix * f_ndc * u_far;
+	f_rayPos = vec4(cameraPosition, 1.0); // TODO: Confirm that cameraPosition = eye...
+	f_rayDir = normalize(P - f_rayPos);
+
     float t;
     for (int i = 0; i < MAX_GEOMETRY_COUNT; ++i) {
         if (i >= u_count) {
--- src/rayMarching.js
+++ src/rayMarching.js
@@ -15,6 +15,18 @@ export default function RayMarcher(renderer, scene, camera) {
                 type: 'i',
                 value: 0
             },
+            u_size: {
+                type: 'v2',
+                value: new THREE.Vector2()
+            },
+            u_inverseViewProjectionMatrix: {
+                type: 'm4',
+                value: new THREE.Matrix4()
+            },
+            u_far: {
+                type: 'f',
+                value: 1000.0 // TODO: Get far clip plane from camera
+            }
         },
         vertexShader: require('./glsl/pass-vert.glsl'),
         fragmentShader: require('./glsl/rayMarch-frag.glsl')
@@ -26,6 +38,15 @@ export default function RayMarcher(renderer, scene, camera) {
         render: function(buffer) {
             shaderPass.material.uniforms.u_buffer.value = buffer;
             shaderPass.material.uniforms.u_count.value = buffer.length / PROXY_BUFFER_SIZE;
+
+            // shaderPass.material.uniforms.u_size.value = 
+            // new THREE.Vector2(renderer.getSize().width, renderer.getSize().height);
+
+            // var projMat = camera.projectionMatrix;
+            // var viewMat = camera.matrixWorldInverse;
+
+            // var viewProjMat = projMat.clone().multiply(viewMat); // Cloned in case it saves to projMat instead
+            // shaderPass.material.uniforms.u_inverseViewProjectionMatrix = viewProjMat.getInverse(viewProjMat);
 
             composer.render();
         }
